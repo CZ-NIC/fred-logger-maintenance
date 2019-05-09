f4646167ccab3ffc1959004dd40e558c638a7866
Jan Musílek <jan.musilek@nic.cz>
2019-05-09 10:21:57 +0200

    Ticket #24662 - Add license info

--- setup.py
+++ setup.py
@@ -6,11 +6,22 @@ setup(name='fred-logger-maintenance',
       author='Jan Musilek, CZ.NIC',
       author_email='jan.musilek@nic.cz',
       url='http://www.nic.cz/',
-      license='GNU GPL',
       platforms=['posix'],
       python_requires='>=3.5',
       long_description="CZ.NIC LOGGER_MAINTENANCE",
       packages=find_packages(),
 
       scripts=['create_parts.py', 'drop_parts.py'],
-      )
+
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Database',
+      ])
